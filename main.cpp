c556873e11eb3d0c662fc4a7f4c4a46698891bb1
Manuel config <browcoanie@gmail.com>
2025-11-01 18:00:54 -0400

    creacion del main

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include "funciones.h"
+#include "utility.h"
+#include "turnos.h"
+#include <windows.h>
+
+
+int main(){
+    return 0;
+}
